7176127df02b793a4af03e00bec860654f54e77a
Jammer1510 <76716282+Jammer1510@users.noreply.github.com>
2022-12-20 15:39:49 -0800

    Delete DesoMobileApp-main directory

--- DesoMobileApp-main/utils/development.js
+++
@@ -1,4 +0,0 @@
-export const API = {
-  'microdatabaseBaseUrl': 'http://10.110.228.226:5000',
-  // 'microdatabaseBaseUrl': 'http://10.110.228.226:5000'
-}
--- DesoMobileApp-main/utils/useUserContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const UserContext = createContext({});
-
-export function useUserContext() {
-  const userContext = useContext(UserContext);
-  return userContext;
-}
